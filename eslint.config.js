9b49d87fa28bd562af3d7956bb0dad9ea6e5c879
Tomas <tomasoldenburg@hotmail.com>
2024-05-03 18:33:39 -0300

    dev: setting typescript-eslint and react-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-// @ts-check
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
 
 export default tseslint.config(
+  { languageOptions: { globals: globals.node } },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  pluginReactConfig
 );
